32f1b170218b22a45ac7a25adcf1b538bf15969f
thomasjodt <jodt5200@gmail.com>
2022-04-21 18:22:21 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const messages = [
+    'Orlando',
+    'Adam',
+    'Javi',
+    'Pibe',
+    'Renzo',
+]
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random()*messages.length)]
+    console.log(message)
+}
+
+module.exports = {randomMsg}
