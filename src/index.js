4720098758f36af9debdb50250ef667e05a7010c
thomasjodt <jodt5200@gmail.com>
2022-04-21 18:41:29 -0500

    Adding a README file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const messages = [
     'Javi',
     'Pibe',
     'Renzo',
+    'Caroline',
+    'Chell',
+    'Ellis',
+    'Francis'
 ]
 const randomMsg = () => {
     const message = messages[Math.floor(Math.random()*messages.length)]
